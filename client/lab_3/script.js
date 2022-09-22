c6865dd9c6b770671c21d75340b0fd6146eae2f3
MJPulley <mjpulley@outlook.com>
2022-09-22 08:43:41 -0400

    edited js script

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -35,7 +35,7 @@ function moveToNextSlide() {
     and if so, sets your slidePosition to the first index of an array
     if not, set the slidePosition to the current position plus one
   */
-  if(slidePosition === totalSlides - 1){
+  if (slidePosition === totalSlides - 1) {
     slidePosition = 0;
   } else {
     slidePosition += 1;

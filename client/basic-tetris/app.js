ab7de682a029bba57322b6df1182c83dc28fbb0f
MJPulley <mjpulley@outlook.com>
2022-09-29 16:24:46 -0400

    edited tetrominoes

--- client/basic-tetris/app.js
+++ client/basic-tetris/app.js
@@ -10,14 +10,14 @@ document.addEventListener('DOMContentLoaded', () => {
     [width, width + 1, width + 2, width * 2 + 2],
     [1, width + 1, width * 2 + 1, width * 2],
     [width, width * 2, width * 2 + 1, width * 2 + 2]
-  ];
+  ]
 
   const zTetromino = [
     [0, width, width + 1, width * 2 + 1],
     [width + 1, width + 2, width * 2, width * 2 + 1],
     [0, width, width + 1, width * 2 + 1],
     [width + 1, width + 2, width * 2, width * 2 + 1]
-  ];
+  ]
 
   const tTetromino = [
     [1, width, width + 1, width + 2],
@@ -31,25 +31,32 @@ document.addEventListener('DOMContentLoaded', () => {
     [0, 1, width, width + 1],
     [0, 1, width, width + 1],
     [0, 1, width, width + 1]
-  ];
+  ]
 
   const iTetromino = [
     [1, width + 1, width * 2 + 1, width * 3 + 1],
     [width, width + 1, width + 2, width + 3],
     [1, width + 1, width * 2 + 1, width * 3 + 1],
     [width, width + 1, width + 2, width + 3]
-  ];
+  ]
 
-  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 
-  let currentPosition = 4;
-  let current = theTetrominoes[0][0];
+  let currentPosition = 4
+  let currentRotation = 0
+
+  let random = math.floor(math.random()*theTetrominoes.length)
+  let current = theTetrominoes[random][0]
 
   function draw() {
     current.forEach((index) => {
-      squares[currentPosition + index].classList.add('tetromino');
-    });
+      squares[currentPosition + index].classList.add('tetromino')
+    })
   }
-});
 
-draw()
+  function undraw() {
+    current.forEach(index => {
+        squares[currentPosition + index].classList.remove('tetromino')
+    })
+  }
+})

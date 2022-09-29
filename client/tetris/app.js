7c02c054147917e8ca8a88ebccb174ab50af87c2
MJPulley <mjpulley@outlook.com>
2022-09-29 17:01:58 -0400

    edits

--- client/tetris/app.js
+++ client/tetris/app.js
@@ -39,39 +39,3 @@ document.addEventListener('DOMContentLoaded', () => {
     [1, width + 1, width * 2 + 1, width * 3 + 1],
     [width, width + 1, width + 2, width + 3]
   ];
-
-  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
-
-  let currentPosition = 4;
-  let currentRotation = 0;
-
-  let random = Math.floor(Math.random() * theTetrominoes.length);
-  let current = theTetrominoes[random][0];
-
-  function draw() {
-    current.forEach((index) => {
-      squares[currentPosition + index].classList.add('tetromino');
-    });
-  }
-
-  function undraw() {
-    current.forEach((index) => {
-      squares[currentPosition + index].classList.remove('tetromino');
-    });
-  }
-
-  timerId = setInterval(moveDown, 1000)
-  undraw()
-  currentPosition += width
-  draw()
-
-  function freeze() {
-    if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
-        current.forEach(index => squares[currentPosition + index].classList.add('taken'))
-        random = Math.random() * theTetrominoes.length)
-        current = theTetrominoes[random][currentRotation]
-        currentPosition = 4
-        draw()
-    }
-  }
-});
